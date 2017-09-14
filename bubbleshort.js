bd0827d397d2775493709b1f3b2f70edb8ed9a55
susan chern <olivegreen48@gmail.com>
2017-09-14 15:15:06 -0400

    2nd try

--- bubbleshort.js
+++ bubbleshort.js
@@ -2,17 +2,29 @@ var bubbleSort = function(arr) {
   if (arr.length <= 1) {
     return arr;
   }
-  return swap(arr);
+
+  var swapTracker = true;
+
+  while(swapTracker){
+    swapTracker = false;
+    for (var i = 0; i < arr.length; i+=2) {
+      if (arr[i] > arr[i+1]) {
+       swap(arr, i);
+       swapTracker = true;
+      }
+    }
+
+    return arr
+}
 };
 
-var swap = function(arr) {
+var swap = function(arr, i) {
   var temp;
-  for (var i = 0; i < arr.length-2; i+=2) {
-    if (arr[i] > arr[i+1]) {
+
+
       temp = arr[i];
       arr[i] = arr[i+1];
       arr[i+1] = temp;
-    }
-  }
-  return arr;
+
+
 };
