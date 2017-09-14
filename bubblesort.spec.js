358f3fc82dd48f6f8f6a12f66e505be431a02d42
Anule Ndukwu <anule.ndukwu@gmail.com>
2017-09-14 12:06:13 -0400

    Created tests for single and mult. items

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,5 +1,13 @@
 describe('Bubble Sort', function(){
   it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+    expect(bubbleSort([]) ).toEqual( [] );
+  });
+
+  it('handles single items in an array', function(){
+    expect(bubbleSort([3])).toEqual([3]);
+  });
+
+  it('handles multiple items in an array', function(){
+    expect(bubbleSort([138, 3, 42, 39, 4])).toEqual([3, 4, 39, 42, 138]);
   });
 });
